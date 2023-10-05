b2471077a279c0c8f10d8a152c70d8fed5444434
justinjack77 <justinjack77@gmail.com>
2023-10-06 02:16:47 +0800

    commit 1

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,10 @@ const myADataBase = "mydb";
 
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: myADataBase
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mydb'
 });
 
 
@@ -23,11 +23,9 @@ app.get("/",(req,res)=>{
 });
 
 app.get("/users", (req,res)=>{
-    const sql = "select * from users";
+    const sql = "SELECT * FROM users";
     db.query(sql,(err,data)=>{
-        if(err){
-            return res.json(err);
-        }
+        if(err) return res.json(err);
         return res.json(data);
     });
 });
