c9dc8a23d0aed881dfd4c7a6c872e3cce28c07f8
justinjack77 <justinjack77@gmail.com>
2023-10-08 10:57:53 +0800

    commit 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import "../src/styles/main.scss";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import News from "./pages/News";
@@ -7,23 +7,39 @@ import Transactions from "./pages/Transactions";
 import Settings from "./pages/Settings";
 import Support from "./pages/Support";
 import Sidebar from "./components/Sidebar";
+import Admin from "./pages/Admin";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Sidebar />
-                <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/news" element={<News />} />
-                    <Route path="/performance" element={<Performance />} />
-                    <Route path="/settings" element={<Settings />} />
-                    <Route path="/support" element={<Support />} />
-                    <Route path="/transactions" element={<Transactions />} />
-                </Routes>
+  const [closeMenu, setCloseMenu] = useState(false);
+  const [selectedContent, setSelectedContent] = useState('Dashboard');
+
+  const handleContentChange = (content) => {
+    setSelectedContent(content);
+    setCloseMenu(true);
+  };
+
+  return (
+    <Router>
+      <div className={`d-flex ${closeMenu ? 'toggled' : ''}`}>
+        <Sidebar closeMenu={closeMenu} onContentChange={handleContentChange} />
+        <div className="flex-grow-2 p-4">
+        <div>
+                <h1>Hello Admin</h1>
+                <hr /><hr />
             </div>
-        </Router>
-    );
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/performance" element={<Performance />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/settings" element={<Settings />} />
+            {/* <Route path="/support" element={<Support />} /> */}
+            <Route path="/support" element={<Admin />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+// import LoginPage from "./Account/LoginPage";
+// import Admin from from "./Admin";
 
 const Dashboard = () => {
     return (
         <div>
             <h1>dashboard.</h1>
+            {/* <LoginPage /> */}
+            
         </div>
+        // <LoginPage />
     );
 };
 
